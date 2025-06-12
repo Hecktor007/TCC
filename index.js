6483413bb53fc2d83a8b353d2a1e5c66b9845c0a
Gabriel França <161170309+GabrielFC001@users.noreply.github.com>
2025-06-12 23:49:52 +0000

    ATUALIZADO

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const app = express();
 
@@ -12,4 +13,20 @@ res.sendFile(path.join(__dirname, 'public', 'login.html'));
 } )
 
 
+ app.get('/cadrasto.html', (req, res) => {
+  const sql = `
+    CREATE TABLE IF NOT EXISTS usuarios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  cpf TEXT NOT NULL UNIQUE CHECK(length(cpf) = 11),
+  senha TEXT NOT NULL,
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+  `;
+})
+
+
+
 app.listen(3000, console.log("rodando"))
+
