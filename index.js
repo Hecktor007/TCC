41308249e9212145191a6d320a881f5fc4cbcd4f
Gabriel França <161170309+GabrielFC001@users.noreply.github.com>
2025-06-12 22:55:10 +0000

    opa

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get("/",(req, res) =>{
-res.send("hellow wold")
+res.sendFile(path.join(__dirname, 'public', 'login.html'));
 
 } )
 
